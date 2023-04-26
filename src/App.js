105cf76af22384959120e6b1c6a3aab0edc4a1cb
nmachuca <nmachuca@protonmail.com>
2023-04-26 17:12:50 -0230

    feat: unsecured version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Table } from './components/Table';
+import { Footer } from './components/Footer';
+import env from "react-dotenv";
+
+
 function App() {
+
+  const [repository, repositorySet] = React.useState([]);
+  const [repositorySelected, repositorySelectedSet] = React.useState("0");
+  const [commitList, commitListSet] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch( env.BASE_API_URL + 'api/repos').then(response => response.json()).then(response => repositorySet(response.data.repositories));
+  }, []);
+
+  React.useEffect(() => {
+    if (repositorySelected > 0) {
+      fetch(env.BASE_API_URL + 'api/commits/' + repositorySelected).then(response => response.json()).then(response => commitListSet(response.data.commits));
+    }
+  }, [repositorySelected])
+
   return (
-    <div >
-      
+    <div>
+      <Header />
+      <section className='selector-section'>
+        <div className='selector-container'>
+          <select onChange={(e) => repositorySelectedSet(e.target.value)}>
+            <option value="0">Seleccione un repositorio</option>
+            {repository.map((a) => <option value={a.id} key={a.id}>{a.full_name}</option>)}
+          </select>
+        </div>
+      </section>
+      <section className='table-section'>
+        <Table commitList={commitList}/>
+      </section>
+      <Footer />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from './App';
+import './css/reset.css';
+import './css/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
 );
